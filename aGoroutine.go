c05a91601d1c9e9a1909ac28d609598978f96d1b
sangkil <ht191@icon.foundation>
2019-04-01 17:21:14 +0900

    Goroutine

---
+++ aGoroutine.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func namedFunction() {
+	time.Sleep(10000 * time.Microsecond)
+	fmt.Println("Printing from namedFunction!")
+}
+
+func main() {
+	fmt.Println("Chapter 09 - Goroutines.")
+	go namedFunction()
+
+	go func() {
+		fmt.Println("An anonymous function! ")
+	}()
+
+	time.Sleep(10000 * time.Microsecond)
+	fmt.Println("Exiting...")
+}
